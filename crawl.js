f922789687b63970fc8eeaa7c29ba4d40376817c
Zidan Abraham <zidangit07@gmail.com>
2023-03-08 00:48:55 +0800

    move crawlPage to top

--- crawl.js
+++ crawl.js
@@ -1,6 +1,29 @@
 import jsdom from "jsdom";
 const { JSDOM } = jsdom;
 
+export async function crawlPage(currentURL) {
+  console.log(`Actively crawling: ${currentURL}`);
+  try {
+    const response = await fetch(currentURL);
+    if (response.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${response.status}, on page: ${currentURL}`
+      );
+      return;
+    }
+    const contentType = response.headers.get("Content-Type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `Error in fetch with headers: ${contentType}, on page: ${currentURL}`
+      );
+      return;
+    }
+    console.log(await response.text());
+  } catch (err) {
+    console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`);
+  }
+}
+
 export function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -38,31 +61,4 @@ export function normalizeURL(urlString) {
 
   // Using ternary
   return fullURL.endsWith("/") ? fullURL.slice(0, -1) : fullURL;
-}
-
-export async function crawlPage(currentURL) {
-  console.log(`Actively crawling: ${currentURL}`);
-
-  try {
-    const response = await fetch(currentURL);
-
-    if (response.status > 399) {
-      console.log(
-        `Error in fetch with status code: ${response.status}, on page: ${currentURL}`
-      );
-      return;
-    }
-
-    const contentType = response.headers.get("Content-Type");
-    if (!contentType.includes("text/html")) {
-      console.log(
-        `Error in fetch with headers: ${contentType}, on page: ${currentURL}`
-      );
-      return;
-    }
-
-    console.log(await response.text());
-  } catch (err) {
-    console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`);
-  }
 }
